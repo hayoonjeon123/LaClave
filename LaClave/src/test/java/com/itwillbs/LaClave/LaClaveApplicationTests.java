e0b06e6d670ea547ef677a606b9b8fc76b02550c
hayoonjeon123 <gkdbs0309@gmail.com>
2025-12-16 15:02:15 +0900

    올라가나

---
+++ LaClave/src/test/java/com/itwillbs/LaClave/LaClaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwillbs.LaClave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaClaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
