28599509a8d299509f275dea5a3e7a401b9aadae
hayoonjeon123 <gkdbs0309@gmail.com>
2025-12-29 16:37:07 +0900

    올라가나
    올라가나

---
+++ LaClave/src/main/java/com/itwillbs/LaClave/LaClaveApplication.java
@@ -0,0 +1,13 @@
+package com.itwillbs.LaClave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaClaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaClaveApplication.class, args);
+	}//되나
+
+}
