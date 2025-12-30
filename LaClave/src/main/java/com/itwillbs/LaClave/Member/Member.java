c84112ac514ed05c12c49da304db639bdddc50f3
eungyeongsong <songeg1004@naver.com>
2025-12-30 11:35:48 +0900

    25-12-30
    gradle

---
+++ LaClave/src/main/java/com/itwillbs/LaClave/Member/Member.java
@@ -0,0 +1,14 @@
+package com.itwillbs.LaClave.Member;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Member {
+
+}
---
+++ LaClave/src/main/java/com/itwillbs/LaClave/Member/MemberController.java
@@ -0,0 +1,8 @@
+package com.itwillbs.LaClave.Member;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+}
