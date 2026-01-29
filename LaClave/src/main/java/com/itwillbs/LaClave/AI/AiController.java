5f5cdba407bac69335216989b9c00f155303f52b
hayoonjeon123 <gkdbs0309@gmail.com>
2026-01-29 14:02:38 +0900

    260129
    주소변경

--- LaClave/src/main/java/com/itwillbs/LaClave/AI/AiController.java
+++ LaClave/src/main/java/com/itwillbs/LaClave/AI/AiController.java
@@ -18,7 +18,7 @@
 //import lombok.extern.slf4j.Slf4j;
 //
 //@RestController
-//@RequestMapping("/ai")
+//@RequestMapping("/api/orders")
 //@RequiredArgsConstructor
 //@Slf4j
 //public class AiController {
--- LaClave/src/main/java/com/itwillbs/LaClave/PayMent/PaymentController.java
+++ LaClave/src/main/java/com/itwillbs/LaClave/PayMent/PaymentController.java
@@ -22,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/orders")
 @RequiredArgsConstructor
 @Log4j2
 public class PaymentController {
