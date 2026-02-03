9c141205a6e3df929df99b141ec3a6a04bbd02fc
hayoonjeon123 <gkdbs0309@gmail.com>
2026-02-03 11:01:09 +0900

    Update ReviewRepository.java

--- LaClave/src/main/java/com/itwillbs/LaClave/review/ReviewRepository.java
+++ LaClave/src/main/java/com/itwillbs/LaClave/review/ReviewRepository.java
@@ -8,45 +8,43 @@ import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
-    List<Review> findAllByMember_MemberIdxAndStatus(Long memberIdx, String status);
+  List<Review> findAllByMember_MemberIdxAndStatus(Long memberIdx, String status);
 
-    List<Review> findByProduct_ProductIdxAndStatus(Long productIdx, String status);
+  List<Review> findByProduct_ProductIdxAndStatus(Long productIdx, String status);
 
-    @Query("""
-        SELECT AVG(r.score)
-        FROM Review r
-        WHERE r.product.productIdx = :productIdx
-          AND r.status = 'ACTIVE'
-    """)
-    Double getAverageScoreByProduct(@Param("productIdx") Long productIdx);
+  @Query("""
+          SELECT AVG(r.score)
+          FROM Review r
+          WHERE r.product.productIdx = :productIdx
+            AND r.status = 'ACTIVE'
+      """)
+  Double getAverageScoreByProduct(@Param("productIdx") Long productIdx);
 
-    @Query("""
-        SELECT COUNT(r)
-        FROM Review r
-        WHERE r.product.productIdx = :productIdx
-          AND r.status = 'ACTIVE'
-    """)
-    Long countActiveReviewsByProduct(@Param("productIdx") Long productIdx);
+  @Query("""
+          SELECT COUNT(r)
+          FROM Review r
+          WHERE r.product.productIdx = :productIdx
+            AND r.status = 'ACTIVE'
+      """)
+  Long countActiveReviewsByProduct(@Param("productIdx") Long productIdx);
 
-    boolean existsByMember_MemberIdxAndOrdersIdxAndProduct_ProductIdx(
-        Long memberIdx,
-        Integer ordersIdx,
-        Long productIdx
-    );
-    
-    @Query("""
-    	    SELECT COUNT(r)
-    	    FROM Review r
-    	    WHERE r.product.productIdx = :productIdx
-    	      AND r.status = 'ACTIVE'
-    	""")
-    	Integer countByProductIdx(@Param("productIdx") Long productIdx);
-    
-    @Query("""
-    	    SELECT r
-    	    FROM Review r
-    	    WHERE r.product.productIdx = :productIdx
-    	""")
-    	List<Review> findByProductIdx(@Param("productIdx") Integer productIdx);
+  boolean existsByMember_MemberIdxAndOrdersIdxAndProduct_ProductIdx(
+      Long memberIdx,
+      Integer ordersIdx,
+      Long productIdx);
+
+  @Query("""
+          SELECT COUNT(r)
+          FROM Review r
+          WHERE r.product.productIdx = :productIdx
+            AND r.status = 'ACTIVE'
+      """)
+  Integer countByProductIdx(@Param("productIdx") Long productIdx);
+
+  @Query("""
+          SELECT r
+          FROM Review r
+          WHERE r.product.productIdx = :productIdx
+      """)
+  List<Review> findByProductIdx(@Param("productIdx") Integer productIdx);
 }
-
